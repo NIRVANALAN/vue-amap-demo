e43cca75fcde3a6f10bda566125d24ca3818d461
newcoderlife <newcoderlife@outlook.com>
2019-05-06 15:07:47 +0800

    create amap object

---
+++ src/components/SubwayMap.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="mysubway"></div>
+</template>
+<script>
+export default {
+  name: "SubwayMap",
+  mounted() {
+    window.addEventListener("load", () => {
+      var mysubway = window.subway("mysubway", { easy: 1 });
+    });
+  }
+};
+</script>
+<style>
+</style>
