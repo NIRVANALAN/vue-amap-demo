17c5ea223f434d016af7783db834acd5d49576dd
newcoderlife <newcoderlife@outlook.com>
2019-05-06 15:15:13 +0800

    add info window example

--- src/components/SubwayMap.vue
+++ src/components/SubwayMap.vue
@@ -5,8 +5,14 @@
 export default {
   name: "SubwayMap",
   mounted() {
+    var mysubway = null;
     window.addEventListener("load", () => {
-      var mysubway = window.subway("mysubway", { easy: 1 });
+      mysubway = window.subway("mysubway", { easy: 1 });
+      mysubway.event.on("subway.complete", () => {
+        mysubway.addInfoWindow("南锣鼓巷");
+        var center = mysubway.GetStCenter("南锣鼓巷");
+        mysubway.setCenter(center);
+      });
     });
   }
 };
