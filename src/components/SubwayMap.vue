1368135ec0a26c84af98a60fdfbb34dcfaa84947
newcoderlife <newcoderlife@outlook.com>
2019-05-06 15:20:00 +0800

    add station marker example

--- src/components/SubwayMap.vue
+++ src/components/SubwayMap.vue
@@ -9,9 +9,25 @@ export default {
     window.addEventListener("load", () => {
       mysubway = window.subway("mysubway", { easy: 1 });
       mysubway.event.on("subway.complete", () => {
+        /* Info window example
         mysubway.addInfoWindow("南锣鼓巷");
         var center = mysubway.GetStCenter("南锣鼓巷");
         mysubway.setCenter(center);
+        */
+        var marker1 = mysubway.addMarker("南锣鼓巷", {});
+        var marker2 = mysubway.addMarker("北海北", {
+          width: 19,
+          height: 33,
+          offset: {
+            x: 0,
+            y: 0
+          },
+          cnt: `
+          <div>
+            <img src="https://webapi.amap.com/theme/v1.3/markers/n/mark_bs.png">
+          </div>
+          `
+        });
       });
     });
   }
